99b5f76357b321dd6ead4d5c4ca70bc90e4cf519
appleoda <oda@apple-osaka.com>
2022-02-28 13:40:14 +0900

    項目をへらす

--- test00/sketch.js
+++ test00/sketch.js
@@ -1,32 +1,37 @@
-// おみくじの内容
-const omikuji = ["大吉", "吉", "中吉", "小吉", "末吉", "凶"];
-// おみくじの結果
-let omikujiResult;
+// 項目の内容
+const textList = ["当たり", "ハズレ", "もう一回"];
+// 選ばれた項目
+let textResult;
 
+// --- 初回更新 ---------------------------------------------------
 function setup() {
+    // 表示するキャンバスを全画面に
     createCanvas(windowWidth,windowHeight);
 
     // 文字の基準点を上下左右の中央に
     textAlign(CENTER, CENTER);
     // 文字の大きさの設定
-    textSize(100);
+    textSize(200);
 
-    // おみくじの結果をランダムにセット
-    omikujiResult = random(omikuji);
+    // 項目の結果をランダムにセット
+    textResult = random(textList);
 }
 
+// --- 1秒間に60回更新 ---------------------------------------------
 function draw() {
+    // 背景を赤色に
     background("#e02020");
-
+    // 塗りつぶしは白色に
     fill("#FFFFFF");
+    // 境界線も白色に
     stroke("#FFFFFF");
   
-    // おみくじの結果を表示
-    text(omikujiResult, width / 2, height / 2);
+    // 選ばれた項目の表示
+    text(textResult, width / 2, height / 2);
 
     // クリックされている間の処理
     if (mouseIsPressed == true) {
-        // おみくじの結果をランダムにセット
-        omikujiResult = random(omikuji);
+        // 項目の結果をランダムにセット
+        textResult = random(textList);
     }
 }
